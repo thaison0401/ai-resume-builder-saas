0727829fac574b9e7137e4ee9c5cb6c6841d3b44
thaisonlun <thaisonlun553@gmail.com>
2026-03-28 13:35:10 +0700

    03: foundation

--- src/components/ui/breadcrumb.tsx
+++ src/components/ui/breadcrumb.tsx
@@ -1,7 +1,7 @@
 import * as React from "react";
 import { Slot } from "radix-ui";
 
-import { cn } from "@/src/lib/utils";
+import { cn } from "@/lib/utils";
 import { ChevronRightIcon, MoreHorizontalIcon } from "lucide-react";
 
 function Breadcrumb({ className, ...props }: React.ComponentProps<"nav">) {
@@ -20,7 +20,7 @@ function BreadcrumbList({ className, ...props }: React.ComponentProps<"ol">) {
     <ol
       data-slot="breadcrumb-list"
       className={cn(
-        "flex flex-wrap items-center gap-1.5 text-sm wrap-break-word text-muted-foreground",
+        "text-muted-foreground flex flex-wrap items-center gap-1.5 text-sm wrap-break-word",
         className,
       )}
       {...props}
@@ -50,7 +50,7 @@ function BreadcrumbLink({
   return (
     <Comp
       data-slot="breadcrumb-link"
-      className={cn("transition-colors hover:text-foreground", className)}
+      className={cn("hover:text-foreground transition-colors", className)}
       {...props}
     />
   );
@@ -63,7 +63,7 @@ function BreadcrumbPage({ className, ...props }: React.ComponentProps<"span">) {
       role="link"
       aria-disabled="true"
       aria-current="page"
-      className={cn("font-normal text-foreground", className)}
+      className={cn("text-foreground font-normal", className)}
       {...props}
     />
   );
